f3f1502c8fdf2e7d9ed48add026aa29f1b2388c0
MennaEssa <m3ess4@gmail.com>
2018-04-15 20:20:02 +0200

    feature: aria labels to home page

--- js/main.js
+++ js/main.js
@@ -159,6 +159,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute('aria-label',"name" + restaurant.name + "address" +restaurant.address + "Details");
   li.append(more)
 
   return li
